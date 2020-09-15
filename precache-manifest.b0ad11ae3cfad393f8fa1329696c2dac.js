8e385c48051dbdef703484aed09ca4e1b25c0834
TheVinh <binkool281132442@gmail.com>
2020-09-15 18:59:37 +0700

    Updates

---
+++ precache-manifest.b0ad11ae3cfad393f8fa1329696c2dac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc961bbe1622e38f79f15620403c2099",
+    "url": "/todoPlan/index.html"
+  },
+  {
+    "revision": "c6614ed0f293fcd06759",
+    "url": "/todoPlan/static/css/main.20c8737a.chunk.css"
+  },
+  {
+    "revision": "56f7db70a0b3ca6a112a",
+    "url": "/todoPlan/static/js/2.b60a2c7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoPlan/static/js/2.b60a2c7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6614ed0f293fcd06759",
+    "url": "/todoPlan/static/js/main.9b18ff46.chunk.js"
+  },
+  {
+    "revision": "ca797335c5e43549ca40",
+    "url": "/todoPlan/static/js/runtime-main.52a7bcbd.js"
+  }
+]);
